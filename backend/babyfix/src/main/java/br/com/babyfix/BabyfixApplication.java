908e9b3591ad36b86dfface2f9c5a2a493d2bd1a
williamcamasil <williamcamargosilva@gmail.com>
2022-03-19 16:57:53 -0300

    feat: create initial backend and frontend

---
+++ backend/babyfix/src/main/java/br/com/babyfix/BabyfixApplication.java
@@ -0,0 +1,13 @@
+package br.com.babyfix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabyfixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabyfixApplication.class, args);
+	}
+
+}
---
+++ backend/babyfix/src/test/java/br/com/babyfix/BabyfixApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.babyfix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabyfixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
